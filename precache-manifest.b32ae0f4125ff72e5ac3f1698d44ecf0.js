52ba17cc21c4d1c38ccd22e48c47163781805e43
bkRahul <rahulsatya1068@gmail.com>
2019-06-14 23:58:08 +0530

    Updates

---
+++ precache-manifest.b32ae0f4125ff72e5ac3f1698d44ecf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02499af94a9b7c45817dab8c8af2747",
+    "url": "/new_recipes/index.html"
+  },
+  {
+    "revision": "41cec0bfededdf624e14",
+    "url": "/new_recipes/static/css/2.ea4892eb.chunk.css"
+  },
+  {
+    "revision": "7891d6cf9616f7196cb7",
+    "url": "/new_recipes/static/css/main.7447b96c.chunk.css"
+  },
+  {
+    "revision": "41cec0bfededdf624e14",
+    "url": "/new_recipes/static/js/2.61509d88.chunk.js"
+  },
+  {
+    "revision": "7891d6cf9616f7196cb7",
+    "url": "/new_recipes/static/js/main.e9eb6eaf.chunk.js"
+  },
+  {
+    "revision": "0286b04c2319076a0996",
+    "url": "/new_recipes/static/js/runtime~main.2284469e.js"
+  }
+]);
